1d96013da728fbf5307c630f72637faf586c8cb1
Fishe90 <dillon.f617@gmail.com>
2021-03-09 22:22:19 -0600

    Added the Fibonacci Sum of Evens

---
+++ FibonacciEvenSum/FibonacciEvenSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int firstNum = 1;
+            int secondNum = 0;
+            int totalSumEven = 0;
+            int totalSumAll = firstNum + secondNum;
+
+            while (totalSumAll < 4000000)
+            {
+                firstNum = totalSumAll - firstNum;
+                secondNum = totalSumAll;
+                totalSumAll = firstNum + secondNum;
+                if (totalSumAll%2 == 0)
+                {
+                    totalSumEven += totalSumAll;
+                }
+                Console.WriteLine(totalSumAll);
+            }
+
+            Console.WriteLine($"Sum of ALL Numbers in the Sequence: {totalSumAll}");
+            Console.WriteLine($"Sum of EVEN Numbers in the Sequence: {totalSumEven}");
+        }
+    }
+}
